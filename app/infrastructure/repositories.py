0815580ea9127a557d4a55d1e22434da9cfb2461
Gustavo de Oliveira <100323079+llGustavsson@users.noreply.github.com>
2026-05-07 14:05:15 -0300

    repository added to group databse methods  Changes to be committed: 	new file:   app/infrastructure/repositories.py

---
+++ app/infrastructure/repositories.py
@@ -0,0 +1,45 @@
+from sqlalchemy.orm import Session
+from infrastructure.orm_models import User, Product, Order, OrderItem
+
+class AppRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    # User Methods
+    def get_user_by_email(self, email: str) -> User:
+        return self.db.query(User).filter(User.email == email).first()
+        
+    def get_user_by_id(self, user_id: int) -> User:
+        return self.db.query(User).filter(User.id == user_id).first()
+
+    def save_user(self, user: User):
+        self.db.add(user)
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+
+    # Product Methods
+    def get_available_products(self):
+        return self.db.query(Product).filter(Product.is_available == True).all()
+        
+    def get_product_by_id(self, product_id: int) -> Product:
+        return self.db.query(Product).filter(Product.id == product_id).first()
+
+    # Order Methods
+    def create_order(self, order: Order, items: list[OrderItem]):
+        self.db.add(order)
+        self.db.flush() # Get order ID without committing yet
+        for item in items:
+            item.order_id = order.id
+            self.db.add(item)
+        self.db.commit()
+        self.db.refresh(order)
+        return order
+        
+    def get_order_by_id(self, order_id: int) -> Order:
+        return self.db.query(Order).filter(Order.id == order_id).first()
+        
+    def update_order(self, order: Order):
+        self.db.commit()
+        self.db.refresh(order)
+        return order
